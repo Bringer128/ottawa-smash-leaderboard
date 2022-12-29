724848bfad6839f3e20949c88e71da4be053d5f8
Ryan Musgrave <ryanm128@gmail.com>
2022-12-29 00:30:34 -0500

    typo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import functions from "@google-cloud/functions-framework";
 
-import { register } from "./register";
+import { register } from "./register.js";
 import { recurringScrape } from "./recurring-scrape.js";
 
 functions.http("register", register);
