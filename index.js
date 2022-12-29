b011883c6ee7dfea95e7c22216746caad4dddf2b
Ryan Musgrave <ryanm128@gmail.com>
2022-12-28 23:22:24 -0500

    Small refactor

--- index.js
+++ index.js
@@ -4,30 +4,17 @@ import { scrape } from "./scrape.js";
 import { createUser } from "./db.js";
 import { discordAuth } from "./discord-auth.js";
 
-functions.http("register", async (req, res) => {
-  console.log("received message");
-  const error = await discordAuth(req.headers, req.rawBody);
-  if (error && error.code) {
-    console.log("Le error: " + error.code);
-    res.status(error.code);
-    return;
-  }
-  if (req.body.type == 1) {
-    res.json({ type: 1 });
-    return;
-  }
-
-  console.log(JSON.stringify(req.body));
+async function doRegister(body) {
   const interestingFields = {
-    channelId: req.body.channel_id,
-    guildId: req.body.data.guild_id,
-    connectCode: req.body.data.options[0].value,
+    channelId: body.channel_id,
+    guildId: body.data.guild_id,
+    connectCode: body.data.options[0].value,
     discordUser: {
-      username: req.body.member.user.username,
-      id: req.body.member.user.id,
-      guildNickname: req.body.member.nick,
+      username: body.member.user.username,
+      id: body.member.user.id,
+      guildNickname: body.member.nick,
     },
-    requestId: req.body.id,
+    requestId: body.id,
   };
 
   const validationError = validate(interestingFields.connectCode);
@@ -43,7 +30,6 @@ functions.http("register", async (req, res) => {
   try {
     await createUser(interestingFields);
 
-    console.log("Success!");
     // Send an HTTP response
     res.json({
       type: 4, // TODO: This is an inline response but we can do a deferred response in case the db is slow
@@ -52,10 +38,28 @@ functions.http("register", async (req, res) => {
       },
     });
   } catch (e) {
-    console.log("Error!", e);
     throw e; // Raise 500
   }
-});
+}
+
+async function register(req, res) {
+  const error = await discordAuth(req.headers, req.rawBody);
+  if (error && error.code) {
+    res.status(error.code);
+    return;
+  }
+  if (req.body.type == 1) {
+    res.json({ type: 1 });
+    return;
+  }
+  if (req.body.type == 2) {
+    await doRegister(req.body, res);
+    return;
+  }
+  res.status(400).json({ error: `Unknown type: ${req.body.type}` });
+}
+
+functions.http("register", register);
 
 function validate(connectCode) {
   if (!connectCode.trim().match(/^[A-Za-z]{1,4}#\d{1,3}/)) {
