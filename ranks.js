71d0f417f51bebb27d4b372cb8e5b3f95c9c391e
Ryan Musgrave <ryanm128@gmail.com>
2023-01-01 01:16:54 -0500

    WIP ranking logic

---
+++ ranks.js
@@ -0,0 +1,48 @@
+const ranges = [
+  ["Bronze I", 0, 765.42],
+  ["Bronze II", 765.43, 913.71],
+  ["Bronze III", 913.72, 1054.86],
+  ["Silver I", 1054.87, 1188.87],
+  ["Silver II", 1188.88, 1315.74],
+  ["Silver III", 1315.75, 1435.47],
+  ["Gold I", 1435.48, 1548.06],
+  ["Gold II", 1548.07, 1653.51],
+  ["Gold III", 1653.52, 1751.82],
+  ["Platinum I", 1751.83, 1842.99],
+  ["Platinum II", 1843, 1927.02],
+  ["Platinum III", 1927.03, 2003.91],
+  ["Diamond I", 2003.92, 2073.66],
+  ["Diamond II", 2073.67, 2136.27],
+  ["Diamond III", 2136.28, 2191.74],
+  ["Master I", 2191.75, 2274.99],
+  ["Master II", 2275, 2350],
+  ["Master III", 2350, Infinity],
+];
+
+export function getRank(
+  numGames,
+  rating,
+  dailyGlobalPlacement,
+  dailyRegionalPlacement
+) {
+  if (numGames === 0) return "None";
+  if (numGames < 5) return "Pending";
+  if (
+    rating >= 2191.75 &&
+    (dailyGlobalPlacement != null || dailyRegionalPlacement != null)
+  ) {
+    return "Grandmaster";
+  }
+
+  ranges.reduce((rating, [rank, min, max]) => {
+    if (rating !== "Unknown") return rating;
+    if (min < rating && rating < max) return rank;
+    return rating;
+  }, "Unknown");
+
+  ranges.forEach(([rank, min, max]) => {
+    if (min <= rating && rating <= max) {
+      return rank;
+    }
+  });
+}
