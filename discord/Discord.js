82eeef1222a04bc83c51c2b6eb713164f6ad38d6
Ryan Musgrave <ryanm128@gmail.com>
2023-01-07 19:35:54 -0500

    Edit message now uses discord class

--- discord/Discord.js
+++ discord/Discord.js
@@ -26,6 +26,30 @@ class Discord {
     return id;
   }
 
+  async editMessage(id, content) {
+    const url = `https://discord.com/api/v10/channels/${channelId}/messages/${id}`;
+    const headers = {
+      Authorization: `Bot ${apiToken}`,
+      "Content-Type": "application/json",
+    };
+    await fetch(url, {
+      headers,
+      method: "PATCH",
+      content,
+    });
+  }
+
+  async getLastMessageInChannel(channelId, apiToken) {
+    const url = `https://discord.com/api/v10/channels/${channelId}`;
+    const headers = {
+      Authorization: `Bot ${apiToken}`,
+      "Content-Type": "application/json",
+    };
+    const response = await fetch(url, { headers });
+    const json = response.json();
+    return json.last_message_id;
+  }
+
   async defineCommand(command) {
     const url = `https://discord.com/api/v10/applications/${APPLICATION_ID}/guilds/${guildId}/commands`;
 
--- edit-message.js
+++ edit-message.js
@@ -1,33 +1,21 @@
-import fetch from "node-fetch";
 import { readResults, readLastMessages } from "./db.js";
 import { formatToMessages } from "./message-formatter.js";
 import { RateLimiter } from "limiter";
-import { createDiscordMessage } from "./create-message.js";
+import Discord from "./discord/Discord.js";
 
 const limiter = new RateLimiter({ tokensPerInterval: 1, interval: "second" });
 
 const TEST_SERVER_CHANNEL_ID = "1057467076639473768";
 const CHANNEL_ID = "1058962408749682698";
 
-async function getLastMessageInChannel(channelId, apiToken) {
-  const url = `https://discord.com/api/v10/channels/${channelId}`;
-  const headers = {
-    Authorization: `Bot ${apiToken}`,
-    "Content-Type": "application/json",
-  };
-
-  const response = await fetch(url, { headers });
-  const json = response.json();
-  return json.last_message_id;
-}
-
-export async function editLastDiscordMessage() {
+export async function editLastDiscordMessages() {
   const apiToken = process.env.BOT_TOKEN;
   if (!apiToken) throw "Requires environment variable: BOT_TOKEN";
 
   const channelId = CHANNEL_ID;
+  const discord = Discord.new({ channelId, apiToken });
 
-  const lastMessageId = await getLastMessageInChannel(channelId, apiToken);
+  const lastMessageId = await discord.getLastMessageInChannel();
 
   const results = await readResults();
   const messages = formatToMessages(results.results);
@@ -41,22 +29,17 @@ export async function editLastDiscordMessage() {
       id,
       content: messages[index],
     }));
+
     for (let { id, content } of updates) {
       await limiter.removeTokens(1);
-      const url = `https://discord.com/api/v10/channels/${channelId}/messages/${id}`;
-      const headers = {
-        Authorization: `Bot ${apiToken}`,
-        "Content-Type": "application/json",
-      };
-      await fetch(url, {
-        headers,
-        method: "PATCH",
-        content,
-      });
+      discord.editMessage(id, content);
     }
   } else {
     // We must create - we're not the latest, or the number of messages has changed
     // so we can't just go back and edit them.
-    await createDiscordMessage();
+    for (let message of messages) {
+      await limiter.removeTokens(1);
+      discord.createMessage(message);
+    }
   }
 }
